cc243b038e21e4b13f5d5dad3b1e499be8decd6a
emmanuel-adewunmi <eodedina@byupathway.edu>
2025-06-08 00:48:32 +0100

    Changes added and week 05 assignment fulfilled

--- scripts/review.js
+++ scripts/review.js
@@ -1,7 +1,9 @@
-let reviewCount = localStorage.getItem("reviewCount") || 0;
-reviewCount++;
-localStorage.setItem("reviewCount", reviewCount);
-document.getElementById("reviewCount").textContent = reviewCount;
+document.addEventListener("DOMContentLoaded", () => {
+    let reviewCount = localStorage.getItem("reviewCount") || 0;
+    reviewCount++;
+    localStorage.setItem("reviewCount", reviewCount);
+    document.getElementById("reviewCount").textContent = reviewCount;
 
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+    document.getElementById("currentyear").textContent = new Date().getFullYear();
+    document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+});
